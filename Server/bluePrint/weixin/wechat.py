349a21c18fafb5f18b79ee7a3daa3a2b7ad5ea61
MachineX <1173579139@qq.com>
2022-04-08 20:59:55 +0800

    新增-获取微信access_token接口

--- Server/bluePrint/weixin/wechat.py
+++ Server/bluePrint/weixin/wechat.py
@@ -14,38 +14,46 @@ weChat_bp = Blueprint('weChat', __name__)
 # wxb28f616376c291c5
 # af45f60a585ebb78ac5b7876318c9216
 
-@weChat_bp.route('/wx')
+# @weChat_bp.route('/wx')
+# def weChat():
+#     args = request.args
+#
+#     signature = args.get('signature')
+#
+#     nonce = args.get('nonce')
+#
+#     timestamp = args.get('timestamp')
+#
+#     echoStr = args.get('echostr')
+#     token = 'xunshu4ng202204081618'
+#
+#     args_list = [
+#         token, timestamp, nonce
+#     ]
+#     args_list.sort()
+#     shaObj = sha1()
+#
+#
+#     shaObj.update(args_list[0].encode('utf-8'))
+#     shaObj.update(args_list[1].encode('utf-8'))
+#     shaObj.update(args_list[2].encode('utf-8'))
+#
+#     hashcode = shaObj.hexdigest()
+#     with open('ok.txt', 'w') as f:
+#         f.write(
+#             signature + '\n' + nonce + '\n' + timestamp + '\n' + echoStr + '\n' + token + '\n' + str(
+#                 args_list) + '\n' + hashcode
+#
+#         )
+#     if hashcode == signature:
+#         return echoStr
+#     else:
+#         return ""
+
+
+
+@weChat_bp.route('/wx',methods=['GET','POST'])
 def weChat():
-    args = request.args
-
-    signature = args.get('signature')
-
-    nonce = args.get('nonce')
-
-    timestamp = args.get('timestamp')
-
-    echoStr = args.get('echostr')
-    token = 'xunshu4ng202204081618'
-
-    args_list = [
-        token, timestamp, nonce
-    ]
-    args_list.sort()
-    shaObj = sha1()
-
-
-    shaObj.update(args_list[0].encode('utf-8'))
-    shaObj.update(args_list[1].encode('utf-8'))
-    shaObj.update(args_list[2].encode('utf-8'))
-
-    hashcode = shaObj.hexdigest()
-    with open('ok.txt', 'w') as f:
-        f.write(
-            signature + '\n' + nonce + '\n' + timestamp + '\n' + echoStr + '\n' + token + '\n' + str(
-                args_list) + '\n' + hashcode
-
-        )
-    if hashcode == signature:
-        return echoStr
-    else:
-        return ""
+    if request.method == 'POST':
+        form = request.form
+        print(form)
