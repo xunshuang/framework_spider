04ad439a50c52089b129c731f284fa714267305b
MachineX <1173579139@qq.com>
2022-04-08 17:06:46 +0800

    新增-微信接口

--- Server/bluePrint/wechat.py
+++ Server/bluePrint/wechat.py
@@ -6,7 +6,7 @@ from Server.api.getMachineListApi import *  # 获取随机推荐
 from Server.api.getMediaListApi import *  # 获取信源
 from Server.api.getMachinePage import get_page,get_relation_page
 from Server.api.getNewestNews import * # 获取新闻
-
+import re
 mysqlOBJ = MYSQL(CONFIG=MYSQL_CONFIG,db='machinedb')  # 该视图的专用mysql对象
 
 weChat_bp = Blueprint('weChat', __name__)
@@ -17,12 +17,16 @@ weChat_bp = Blueprint('weChat', __name__)
 @weChat_bp.route('/wx')
 def weChat():
     args = request.args
-    with open('/root/ok.txt','a+') as f:
-        f.write(str(args))
+
     signature = args.get('signature')
-    timestamp = args.get('timestamp')
+
     nonce = args.get('nonce')
-    echoStr = args.get('echostr')
+    echo = args.get('echostr')
+
+
+    timestamp = echo.split('×tamp=')[1]
+
+    echoStr = echo.split('×tamp=')[0]
     token = 'xun123'
 
     args_list = [
@@ -33,8 +37,6 @@ def weChat():
     map(shaObj.update,args_list)
     hashcode = shaObj.hexdigest()
 
-    with open('/root/ok.txt','a+') as f:
-        f.write(hashcode+'$$$$'+signature)
 
     if hashcode == signature:
         return echoStr
