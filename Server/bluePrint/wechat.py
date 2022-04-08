a1096cfbeb8121dcb4405caa23a9658b236652ba
MachineX <1173579139@qq.com>
2022-04-08 16:12:28 +0800

    新增-微信接口

---
+++ Server/bluePrint/wechat.py
@@ -0,0 +1,18 @@
+from flask import Flask, Blueprint, request, jsonify, render_template
+
+from Config.GlobalSetting import MYSQL_CONFIG
+from Server.api.getMachineListApi import *  # 获取随机推荐
+from Server.api.getMediaListApi import *  # 获取信源
+from Server.api.getMachinePage import get_page,get_relation_page
+from Server.api.getNewestNews import * # 获取新闻
+
+mysqlOBJ = MYSQL(CONFIG=MYSQL_CONFIG,db='machinedb')  # 该视图的专用mysql对象
+
+weChat_bp = Blueprint('weChat', __name__)
+
+
+
+
+@weChat_bp.route('/wx')
+def weChat():
+    return 'hello world'
