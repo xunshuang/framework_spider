998143d44a6acdf315e3a400421cab645f24815b
MachineX <1173579139@qq.com>
2022-04-08 21:33:03 +0800

    新增-获取微信access_token接口

--- Server/bluePrint/weixin/wechat.py
+++ Server/bluePrint/weixin/wechat.py
@@ -5,7 +5,6 @@ from xml.etree import ElementTree
 
 from Server.api.pc.getNewestNews import *  # 获取新闻
 
-
 mysqlOBJ = MYSQL(CONFIG=MYSQL_CONFIG, db='machinedb')  # 该视图的专用mysql对象
 
 weChat_bp = Blueprint('weChat', __name__)
@@ -14,11 +13,9 @@ weChat_bp = Blueprint('weChat', __name__)
 # wxb28f616376c291c5
 # af45f60a585ebb78ac5b7876318c9216
 
-@weChat_bp.route('/wx',methods=['GET','POST'])
+@weChat_bp.route('/wx', methods=['GET', 'POST'])
 def weChat():
-    print('##########', request.args)
     print('##########', request.data)
-    print('##########', request.form)
 
     args = request.args
 
@@ -43,11 +40,18 @@ def weChat():
 
     if hashcode == signature:
 
+        if request.data:
+            XMLData = request.data.decode('utf-8')
+            ElementTree.parse(XMLData)
 
-        print(123)
-        return echoStr
+        return """
+        <xml>
+          <ToUserName><![CDATA[oKV3l6GA5S1Bnnakk_ThJvqdbbIA]]></ToUserName>
+          <FromUserName><![CDATA[gh_dcd30c3d7c29]]></FromUserName>
+          <CreateTime>12345678</CreateTime>
+          <MsgType><![CDATA[text]]></MsgType>
+          <Content><![CDATA[你好]]></Content>
+        </xml>
+        """
     else:
         return ""
-
-
-
