8dc32ccc851d750e56ae81a1bd737e672337243a
MachineX <1173579139@qq.com>
2022-04-08 16:30:42 +0800

    新增-微信接口

--- Server/bluePrint/wechat.py
+++ Server/bluePrint/wechat.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint, request, jsonify, render_template
-
+from hashlib import sha1
 from Config.GlobalSetting import MYSQL_CONFIG
 from Server.api.getMachineListApi import *  # 获取随机推荐
 from Server.api.getMediaListApi import *  # 获取信源
@@ -15,4 +15,20 @@ weChat_bp = Blueprint('weChat', __name__)
 
 @weChat_bp.route('/wx')
 def weChat():
-    return 'hello world'
+    args = request.args
+    signature = args.get('signature')
+    timestamp = args.get('timestamp')
+    nonce = args.get('nonce')
+    echoStr = args.get('echostr')
+    token = 'xunshu4ng202204081618'
+
+    args_list = [
+        token,timestamp,nonce
+    ].sort()
+    shaObj = sha1()
+    map(shaObj.update,args_list)
+    hashcode = shaObj.hexdigest()
+    if hashcode == signature:
+        return echoStr
+    else:
+        return ""
