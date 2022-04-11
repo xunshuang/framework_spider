92bafc1c97e02616ef0efb137d132431a80f0878
MachineX <1173579139@qq.com>
2022-04-11 20:37:52 +0800

    修改-图文消息推送频率

--- Server/api/weixin/message/Message_switch.py
+++ Server/api/weixin/message/Message_switch.py
@@ -14,9 +14,12 @@ def message_switch(*args,**kwargs):
     try:
         return MessageFuncMap[Content](mysqlOBJ=mysqlOBJ,xmlDict=xmlDict)
     except:
+        print(traceback.format_exc())
         try:
             return MessageFuncMap['repeat'](mysqlOBJ=mysqlOBJ,xmlDict=xmlDict) # 报错就复读机模式
         except:
+            print(traceback.format_exc())
+
             return "指令读取失败，输入!help(不限中英文标点符号) 查看帮助菜单!" # 复读失败就发送指令集
 
 
