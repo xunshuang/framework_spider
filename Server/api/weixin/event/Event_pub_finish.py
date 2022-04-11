6ae0813a5f7eb6901f7691d38d65ce80abe4a2bd
MachineX <1173579139@qq.com>
2022-04-11 20:43:20 +0800

    修改-图文消息推送频率

--- Server/api/weixin/event/Event_pub_finish.py
+++ Server/api/weixin/event/Event_pub_finish.py
@@ -32,6 +32,7 @@ def get_accessToken(mysqlObj):
 def publish_event(*args, **kwargs):
     mysqlObj = kwargs.get('mysqlObj')
     xmlDict = kwargs.get('xmlDict')
+    print(xmlDict)
     article_id = xmlDict['article_id']
     doc = {
         "article_id": article_id
--- Server/api/weixin/message/Message_repeat.py
+++
@@ -1,8 +0,0 @@
-# coding:utf-8
-# 复读机模式
-
-def repeat(*args,**kwargs):
-    FContent = kwargs.get('FContent')
-    if "收到不支持" in FContent:
-        return "卧槽！你这表情包我没有！"
-    return FContent
